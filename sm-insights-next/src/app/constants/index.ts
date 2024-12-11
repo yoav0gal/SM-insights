0d507d38cdca8e52fe4533e87d5e726e553d4d44
yoav0gal <yoav0gal@gmail.com>
2024-12-11 19:59:35 +0200

    enabled basic login and instagram profile gets

--- sm-insights-next/src/app/constants/index.ts
+++ sm-insights-next/src/app/constants/index.ts
@@ -1,5 +1,8 @@
 export const OAUTH_INSTAGRAM_URL = `https://api.instagram.com/oauth/authorize`;
 export const OAUTH_INSTAGRAM_REDIRECT_URI = `${
   process.env.NEXTAUTH_URL ?? `https://smfront-yoav0gals-projects.vercel.app`
-}/api/auth/callback/instagram`;
-export const OAUTH_INSTAGRAM_SCOPE = `instagram_business_basic`;
+}${false ? "/api/auth/callback/instagram" : "/api/completelogin"}`;
+
+// auth / callback / instagram;
+export const OAUTH_INSTAGRAM_SCOPE = `instagram_business_basic,business_manage_comments,business_content_publish,business_manage_messages`;
+export const MY_PROFILE_PATH = `${process.env.DOMAIN}/main`;

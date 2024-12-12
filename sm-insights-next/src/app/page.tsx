8c2d35dfc3547e2e712a8a9d870defd7f237b440
yoav0gal <yoav0gal@gmail.com>
2024-12-12 09:11:22 +0200

    try fix remote sso

--- sm-insights-next/src/app/page.tsx
+++ sm-insights-next/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { OAUTH_INSTAGRAM_REDIRECT_URI } from "./constants";
 
 export default function Home() {
   return (
@@ -15,13 +16,11 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
+              {OAUTH_INSTAGRAM_REDIRECT_URI}
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">

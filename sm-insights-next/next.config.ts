3a30e5ed312eea06e14a57ef881ad306a6dde849
yoav0gal <yoav0gal@gmail.com>
2024-12-12 09:34:40 +0200

    try none optimize

--- sm-insights-next/next.config.ts
+++ sm-insights-next/next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["scontent.cdninstagram.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scontent.cdninstagram.com",
+        port: "",
+        pathname: "/v/**",
+      },
+    ],
+    unoptimized: true, // Use this as a last resort
   },
 };
 

1f493d5a3cbf133181bac49f9a1fbbc094b03b2f
yoav0gal <yoav0gal@gmail.com>
2025-01-20 14:14:22 +0200

    initial supabase

--- sm-insights-next/src/app/api/google/login/route.ts
+++ sm-insights-next/src/app/api/google/login/route.ts
@@ -1,5 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
+import { supabase } from "@/app/clients/supabase";
 
 export async function GET(request: NextRequest) {
-  return NextResponse.json("google-login!");
+  const { data, error } = await supabase.from("Users").select("*");
+
+  if (error) {
+    console.error("Error fetching data:", error);
+  } else {
+    console.log("Data:", data);
+  }
+
+  return NextResponse.json(data);
 }

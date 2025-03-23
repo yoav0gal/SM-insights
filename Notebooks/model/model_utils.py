a56d1ee291812a198612fab6adef97b1adf9d0be
guy houri <guy.houri2024@gmail.com>
2025-03-23 06:25:51 +0200

    remove models

--- Notebooks/model/model_utils.py
+++ Notebooks/model/model_utils.py
@@ -6,13 +6,21 @@ from sklearn.feature_extraction.text import CountVectorizer
 import os
 
 bertopic_model_file = "bertopic_model.pkl"
-sentence_transformer_name = "sentence_transformer"
+sentence_transformer_name = "sentence_transformer_model"
 models_dir = os.path.dirname(os.path.abspath(__file__))
 bertopic_model_path = f'{models_dir}/{bertopic_model_file}'
 sentence_transformer_path = f'{models_dir}/{sentence_transformer_name}'
 
+def create_sentence_transformer_model() -> SentenceTransformer:
+    model = SentenceTransformer('all-mpnet-base-v2')
+    save_sentence_transformer_model(model)
+    return model
+
 def get_sentence_transformer_model() -> SentenceTransformer:
-    return SentenceTransformer(sentence_transformer_path)
+    if os.path.exists(sentence_transformer_path):
+        return SentenceTransformer(sentence_transformer_path)
+    else:
+        return create_sentence_transformer_model()
 
 def save_sentence_transformer_model(model: SentenceTransformer):
     model.save(sentence_transformer_path)
@@ -34,5 +42,8 @@ def create_model() -> BERTopic:
     return bertopic_model
 
 def get_model() -> BERTopic:
-    with open(bertopic_model_path, "rb") as file:
-        return pickle.load(file)
+    if os.path.exists(bertopic_model_path):
+        with open(bertopic_model_path, "rb") as file:
+            return pickle.load(file)
+    else:
+        return create_model()
--- Notebooks/model/sentence_transformer/README.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ab326e6bb3798d1248f7b387bc3fd1710966c61aff3a5d0fb2a960f61ccb32cf
-size 10587

e54a72836c9909036b6520f5fa45a3d701ee049c
guy houri <guy.houri2024@gmail.com>
2025-03-25 05:54:03 +0200

    add twitter datasets

--- Notebooks/data/data_utils.py
+++ Notebooks/data/data_utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 def get_comments(dataset_name: str) -> List[str]:
-    dataset_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "youtube_comments", f'{dataset_name}.csv')
+    dataset_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "datasets", "youtube_comments", f'{dataset_name}.csv')
     dataset_df = pd.read_csv(dataset_path)
     data = dataset_df['text'].tolist()
 

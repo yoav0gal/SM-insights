29a85046bf2ad8908a399739040c0527283b63af
guyHouri <guy.houri2024@gmail.com>
2025-07-26 10:44:15 +0300

    merge with main

--- final-project-next/src/app/components/skeleton.tsx
+++ final-project-next/src/app/components/skeleton.tsx
@@ -1,11 +1,11 @@
+import React from "react";
+import "./skeleton.css";
+
 interface SkeletonProps {
   className?: string;
+  style?: React.CSSProperties;
 }
 
-export function Skeleton({ className }: SkeletonProps) {
-  return (
-    <div
-      className={`animate-pulse bg-gray-200 dark:bg-gray-700 rounded ${className}`}
-    />
-  );
+export function Skeleton({ className = "", style }: SkeletonProps) {
+  return <div className={`skeleton-loader ${className}`} style={style} />;
 }
